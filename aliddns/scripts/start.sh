95fdfe4939cd68e3bb8c9f4ef96344b34b073b33
neroxps <neroxps@gmail.com>
2018-04-20 17:33:39 +0800

    fix bug

--- aliddns/scripts/start.sh
+++ aliddns/scripts/start.sh
@@ -5,11 +5,11 @@ AKID="$(jq -r ".akid" $OPTIONS )"
 AKSCT="$(jq -r ".aksct" $OPTIONS )"
 DOMAIN="$(jq -r ".domain" $OPTIONS )"
 REDO="$(jq -r ".redo" $OPTIONS )"
-if [[ "$REDO" == "" ]]; then
+if [[ "$REDO" == "null" ]]; then
 	REDO="600"
 fi
 IPAPI="$(jq -r ".ipapi" $OPTIONS )"
-if [[ "$IPAPI" == "" ]]; then
+if [[ "$IPAPI" == "null" ]]; then
 	IPAPI="[IPAPI-GROUP]"
 fi
 
