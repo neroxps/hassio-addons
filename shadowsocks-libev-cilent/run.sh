bf01d3d05205c28d620fa3e2ff6fc498917dd192
neroxps <neroxps@gmail.com>
2018-10-24 10:35:25 +0800

    fixed run.sh

--- shadowsocks-libev-cilent/run.sh
+++ shadowsocks-libev-cilent/run.sh
@@ -3,7 +3,7 @@ options="/data/options.json"
 
 # 将 json 的 key 作为变量名，value 作为内容，历遍赋值给变量。
 keys=$(jq -r '. | keys' ${options} | sed 's/\[\|\]\|\"\|\,//g'| xargs)
-for i in $keys;do eval $i=$(jq -r --arg i $i ".$i" ${options}) ;done
+for i in $keys;do eval $i=$(jq -r --arg i $i ".$i | @sh" ${options}) ;done
 
 /usr/bin/ss-local  \
 	-s ${server} \
